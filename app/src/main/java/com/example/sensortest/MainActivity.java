2f460dbfb48db948559b8781bd1e169119562796
Felipe <felipe.salvatierra@dydyo.com.br>
2022-04-30 18:38:21 -0400

    first commit

---
+++ app/src/main/java/com/example/sensortest/MainActivity.java
@@ -0,0 +1,298 @@
+package com.example.sensortest;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.BatteryManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CpuUsageInfo;
+import android.os.HardwarePropertiesManager;
+import android.provider.SyncStateContract;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Pattern;
+
+import static android.os.HardwarePropertiesManager.DEVICE_TEMPERATURE_CPU;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private TextView textTemperatura;
+    private TextView falloText;
+    private SensorManager mSensorManager;
+    private Sensor mTemperatura;
+    private Boolean sensorDisponible;
+    private int temperatura = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        textTemperatura = findViewById(R.id.temperatura);
+        falloText = findViewById(R.id.fallo);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        /*float[] coreValues = new float[10];
+        //get how many cores there are from function
+        int numCores = getNumCores();
+        for(byte i = 0; i < numCores; i++)
+        {
+            coreValues[i] = readCore(i);
+            coreValues[i] = coreValues[i] * 100;
+            textTemperatura.append("NÚCLEOS " + coreValues[i] + "\n");
+        }*/
+
+        temp_cpu();
+        Timer();
+
+        /*HardwarePropertiesManager hpm = getApplicationContext().getSystemService(HardwarePropertiesManager.class);
+        float[] celcius = hpm.getDeviceTemperatures(DEVICE_TEMPERATURE_CPU, HardwarePropertiesManager.TEMPERATURE_CURRENT);
+
+
+        textTemperatura.setText("TEMP BATTERY " + celcius.toString() +
+                "\n" + " TEMP CPU " + DEVICE_TEMPERATURE_CPU +
+                "\n" + " TEMP GPU " + HardwarePropertiesManager.DEVICE_TEMPERATURE_GPU +
+                "\n" + " TEMP SKIN" + HardwarePropertiesManager.DEVICE_TEMPERATURE_SKIN);*/
+
+
+        //Revisa si existe un sensor en el dispositivo
+        /*if (mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) !=null){
+            mTemperatura = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+            sensorDisponible = true;
+            falloText.setVisibility(View.GONE);
+        }else{
+            falloText.setText("Este dispositivo não possui sensor de temperatura");
+            textTemperatura.setVisibility(View.GONE);
+            sensorDisponible = false;
+        }
+
+        if (sensorDisponible){
+            mSensorManager.registerListener(this, mTemperatura, mSensorManager.SENSOR_DELAY_NORMAL);
+        }*/
+    }
+
+    /**
+     * Gets the number of cores available in this device, across all processors.
+     * Requires: Ability to peruse the filesystem at "/sys/devices/system/cpu"
+     * @return The number of cores, or 1 if failed to get result
+     */
+    private int getNumCores() {
+        //Private Class to display only CPU devices in the directory listing
+        class CpuFilter implements FileFilter {
+            @Override
+            public boolean accept(File pathname) {
+                //Check if filename is "cpu", followed by one or more digits
+                if(Pattern.matches("cpu[0-9]+", pathname.getName())) {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        try {
+            //Get directory containing CPU info
+            File dir = new File("/sys/devices/system/cpu/");
+            //Filter to only list the devices we care about
+            File[] files = dir.listFiles((FileFilter) new CpuFilter());
+            //Return the number of cores (virtual CPU devices)
+            return files.length;
+        } catch(Exception e) {
+            //Default to return 1 core
+            return 1;
+        }
+    }
+
+    // for multi core value
+    private float readCore(int i) {
+        /*
+         * how to calculate multicore this function reads the bytes from a
+         * logging file in the android system (/proc/stat for cpu values) then
+         * puts the line into a string then spilts up each individual part into
+         * an array then(since he know which part represents what) we are able
+         * to determine each cpu total and work then combine it together to get
+         * a single float for overall cpu usage
+         */
+        try {
+            RandomAccessFile reader = new RandomAccessFile("/proc/stat", "r");
+            // skip to the line we need
+            for (int ii = 0; ii < i + 1; ++ii) {
+                String line = reader.readLine();
+            }
+            String load = reader.readLine();
+
+            // cores will eventually go offline, and if it does, then it is at
+            // 0% because it is not being
+            // used. so we need to do check if the line we got contains cpu, if
+            // not, then this core = 0
+            if (load.contains("cpu")) {
+                String[] toks = load.split(" ");
+
+                // we are recording the work being used by the user and
+                // system(work) and the total info
+                // of cpu stuff (total)
+                // http://stackoverflow.com/questions/3017162/how-to-get-total-cpu-usage-in-linux-c/3017438#3017438
+
+                long work1 = Long.parseLong(toks[1]) + Long.parseLong(toks[2])
+                        + Long.parseLong(toks[3]);
+                long total1 = Long.parseLong(toks[1]) + Long.parseLong(toks[2])
+                        + Long.parseLong(toks[3]) + Long.parseLong(toks[4])
+                        + Long.parseLong(toks[5]) + Long.parseLong(toks[6])
+                        + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);
+
+                try {
+                    // short sleep time = less accurate. But android devices
+                    // typically don't have more than
+                    // 4 cores, and I'n my app, I run this all in a second. So,
+                    // I need it a bit shorter
+                    Thread.sleep(300);
+                } catch (Exception e) {
+                }
+
+                reader.seek(0);
+                // skip to the line we need
+                for (int ii = 0; ii < i + 1; ++ii) {
+                    reader.readLine();
+                }
+                load = reader.readLine();
+
+                // cores will eventually go offline, and if it does, then it is
+                // at 0% because it is not being
+                // used. so we need to do check if the line we got contains cpu,
+                // if not, then this core = 0%
+                if (load.contains("cpu")) {
+                    reader.close();
+                    toks = load.split(" ");
+
+                    long work2 = Long.parseLong(toks[1]) + Long.parseLong(toks[2])
+                            + Long.parseLong(toks[3]);
+                    long total2 = Long.parseLong(toks[1]) + Long.parseLong(toks[2])
+                            + Long.parseLong(toks[3]) + Long.parseLong(toks[4])
+                            + Long.parseLong(toks[5]) + Long.parseLong(toks[6])
+                            + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);
+
+                    // here we find the change in user work and total info, and
+                    // divide by one another to get our total
+                    // seems to be accurate need to test on quad core
+                    // http://stackoverflow.com/questions/3017162/how-to-get-total-cpu-usage-in-linux-c/3017438#3017438
+
+                    if ((total2 - total1) == 0)
+                        return 0;
+                    else
+                        return (float) (work2 - work1) / ((total2 - total1));
+
+                } else {
+                    reader.close();
+                    return 0;
+                }
+
+            } else {
+                reader.close();
+                return 0;
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return 0;
+    }
+
+    public static float cpuTemperature() {
+        Process process;
+        try {
+            process = Runtime.getRuntime().exec("cat sys/class/thermal/thermal_zone0/temp");
+            process.waitFor();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = reader.readLine();
+            if (line != null) {
+                float temp = Float.parseFloat(line);
+                return temp / 1000.0f;
+            } else {
+                return 45.0f;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0.0f;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        temperatura = (int) event.values[0];
+        textTemperatura.setText(temperatura + " °C");
+        
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void Timer(){
+        Timer timer = new Timer();
+        Task task = new Task();
+
+        timer.schedule(task, 5000, 5000);
+
+    }
+
+    class Task extends TimerTask{
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    //Refresh Activity
+                    overridePendingTransition(0, 0);
+                    finish();
+                    overridePendingTransition(0, 0);
+                    startActivity(getIntent());
+                    overridePendingTransition(0, 0);
+                }
+            });
+        }
+    }
+    public void temp_cpu() {
+            /*RandomAccessFile reader = new RandomAccessFile("/sys/devices/system/cpu/cpu0/cpufreq/cpu_temp", "r");
+            String line = reader.readLine();
+
+            textTemperatura.setText(Integer.parseInt(line) / 1000);
+            textTemperatura.setText("TEMP BATTERY " + HardwarePropertiesManager.DEVICE_TEMPERATURE_BATTERY +
+                    "\n" + " TEMP CPU " + DEVICE_TEMPERATURE_CPU +
+                    "\n" + " TEMP GPU " + HardwarePropertiesManager.DEVICE_TEMPERATURE_GPU +
+                    "\n" + " TEMP SKIN" + HardwarePropertiesManager.DEVICE_TEMPERATURE_SKIN);*/
+
+        float tempo = cpuTemperature();
+        textTemperatura.setText(String.valueOf(tempo) + " Cº");
+
+
+    }
+
+
+}
